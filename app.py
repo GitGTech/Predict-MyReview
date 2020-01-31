b93a40ab0aaa1a4832132baf7ae0d282b82a9c3e
wondermom <vidhyamathers@gmail.com>
2020-01-31 14:02:50 -0500

    initial push

---
+++ app.py
@@ -0,0 +1,169 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template,url_for,request
+from flask_sqlalchemy import SQLAlchemy
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+
+
+
+
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+
+#postgreSQL connection
+# rds_connection_string =  "postgres:5432@localhost:5432/GUN_VIOLENCE_PROJECT" 
+# engine = create_engine(f'postgresql://{rds_connection_string}')
+
+# # bar chart data 
+# @app.route("/yeardata")
+# def year():
+#     df = pd.read_sql_query('SELECT  * FROM guns_year LIMIT 5' , con=engine).head()
+#     bar_list = df.to_dict(orient='records')
+#     print (bar_list)
+#     return jsonify(bar_list)
+###############################################################
+# pie chart data from PostGres
+# @app.route("/shootingtypes")
+# def shootingstypes():
+#      type_df = pd.read_sql_query('select * from gunshootingstype', con=engine).head(10)
+#      shootingstypes = type_df.to_dict(orient='records')
+#      return jsonify(shootingstypes)
+
+
+
+#######################################################
+# Bar Chart data from csv
+# @app.route("/barchartdata")
+# def yearlybarchart():
+#      bar_df = pd.read_csv("static/data/year_trend_gun.csv")
+#      bar_dict = bar_df.to_dict(orient='records')
+#      return jsonify(bar_dict)
+
+#########################################################
+# line chart data from csv
+# @app.route("/monthlydata")
+# def month():
+#      flaskdf = pd.read_csv("static/data/gun2014onwrd.csv")
+#      line_dict = flaskdf.to_dict(orient='records')
+#      return jsonify(line_dict)
+
+#########################################################
+# pie chart fron csv
+# @app.route("/incidents")
+# def incidents ():
+#      incidentdf = pd.read_csv("static/data/Years_Data_2014_2019.csv")
+#      incident_dict = incidentdf.to_dict(orient='records')
+#      return jsonify(incident_dict)
+
+
+
+##########################################################
+# pie chart data from PostGres
+# @app.route("/markercluster")
+# def markercluster():
+#      df = pd.read_sql_query('select * from markercluster', con=engine)
+#      markercluster_dict = df.to_dict(orient='records')
+#      print("Reading from Database")
+#      # print(markercluster_dict)
+#      return jsonify(markercluster_dict)
+
+#######################################################
+
+
+# rendering templates for all html pages
+@app.route("/")
+def index():
+#     """Return the homepage."""
+    return render_template("index.html")
+
+
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+     amazon= pd.read_csv("static/data/amazon_Reviews.csv")
+     X1=amazon['reviews.text'].values.astype('U')
+     y1=amazon['review_rating']
+     X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y1, test_size=0.33, random_state=42)
+
+     vectorizer= TfidfVectorizer()
+     X1_train_vectors=vectorizer.fit_transform(X1_train)
+     X1_test_vectors = vectorizer.transform(X1_test)
+
+
+     model_nb = MultinomialNB()
+     model_nb.fit(X1_train_vectors,y1_train)
+     model_nb.predict(X1_test_vectors)
+
+     filename = 'NaiveBayesModel.pkl'
+     joblib.dump(model_nb, filename)
+     NB_model = open('NaiveBayesModel.pkl','rb')
+     NB_model_loaded = joblib.load(NB_model)
+
+
+    
+    
+    
+     if request.method == "POST":
+          message = request.form["message"]
+          data = [message]
+          vect = vectorizer.transform(data)
+          my_prediction = NB_model_loaded.predict(vect)
+     return render_template('result.html',prediction = my_prediction)
+
+
+@app.route("/data")
+def datatabicon():
+     return render_template("databutton.html")
+
+
+
+@app.route("/precision")
+def precisionicon():
+     return render_template("precision.html")
+
+
+@app.route("/predictor")
+def predictoricon():
+     return render_template("predictor.html")
+
+@app.route("/model")
+def modelicon():
+     return render_template("model.html")
+
+# @app.route("/timeframeheatmap")
+# def heatmap():
+#      return render_template("heatmap.html")
+   
+    
+
+# @app.route("/current2019")
+# def choroplethmap():
+#      return render_template("choroplethmap.html")
+   
+
+# @app.route("/gundata")
+# def data():
+#      return render_template("gundata.html")
+   
+
+if __name__ == "__main__":
+     app.run(debug=True)
+    
