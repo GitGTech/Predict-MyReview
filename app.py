0d8be519c672ac2a806b17aa96032a46814b08f1
wondermom <vidhyamathers@gmail.com>
2020-02-04 09:47:17 -0500

    revised predict results appear on same page

--- app.py
+++ app.py
@@ -17,10 +17,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
 
+amazon= pd.read_csv("static/data/amazon_Reviews.csv")
+X1=amazon['reviews.text'].values.astype('U')
+y1=amazon['review_rating']
+X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y1, test_size=0.33, random_state=42)
+
+vectorizer= TfidfVectorizer()
+X1_train_vectors=vectorizer.fit_transform(X1_train)
+X1_test_vectors = vectorizer.transform(X1_test)
 
 
+model_nb = MultinomialNB()
+model_nb.fit(X1_train_vectors,y1_train)
+model_nb.predict(X1_test_vectors)
 
 app = Flask(__name__)
+
+filename = 'NaiveBayesModel.pkl'
+joblib.dump(model_nb, filename)
+NB_model = open('NaiveBayesModel.pkl','rb')
+NB_model_loaded = joblib.load(NB_model)
+
+
 
 # rendering templates for all html pages
 @app.route("/")
@@ -29,39 +47,51 @@ def index():
     return render_template("index.html")
 
 
-
-
-@app.route('/predict',methods=['POST'])
+@app.route("/predictor",methods=['POST'])
 def predict():
-     amazon= pd.read_csv("static/data/amazon_Reviews.csv")
-     X1=amazon['reviews.text'].values.astype('U')
-     y1=amazon['review_rating']
-     X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y1, test_size=0.33, random_state=42)
-
-     vectorizer= TfidfVectorizer()
-     X1_train_vectors=vectorizer.fit_transform(X1_train)
-     X1_test_vectors = vectorizer.transform(X1_test)
-
-
-     model_nb = MultinomialNB()
-     model_nb.fit(X1_train_vectors,y1_train)
-     model_nb.predict(X1_test_vectors)
-
-     filename = 'NaiveBayesModel.pkl'
-     joblib.dump(model_nb, filename)
-     NB_model = open('NaiveBayesModel.pkl','rb')
-     NB_model_loaded = joblib.load(NB_model)
-
-
-    
-    
-    
      if request.method == "POST":
           message = request.form["message"]
           data = [message]
           vect = vectorizer.transform(data)
           my_prediction = NB_model_loaded.predict(vect)
-     return render_template('result.html',prediction = my_prediction)
+          if my_prediction == 'POSITIVE' :
+               prediction = "This review is POSITIVE"
+
+          if my_prediction == 'NEGATIVE' :
+               prediction = "This review is NEGATIVE"
+
+	
+	
+     return render_template('predictor.html',prediction = prediction)
+
+
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#      amazon= pd.read_csv("static/data/amazon_Reviews.csv")
+#      X1=amazon['reviews.text'].values.astype('U')
+#      y1=amazon['review_rating']
+#      X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y1, test_size=0.33, random_state=42)
+
+#      vectorizer= TfidfVectorizer()
+#      X1_train_vectors=vectorizer.fit_transform(X1_train)
+#      X1_test_vectors = vectorizer.transform(X1_test)
+
+
+#      model_nb = MultinomialNB()
+#      model_nb.fit(X1_train_vectors,y1_train)
+#      model_nb.predict(X1_test_vectors)
+
+#      filename = 'NaiveBayesModel.pkl'
+#      joblib.dump(model_nb, filename)
+#      NB_model = open('NaiveBayesModel.pkl','rb')
+#      NB_model_loaded = joblib.load(NB_model)   
+    
+#      if request.method == "POST":
+#           message = request.form["message"]
+#           data = [message]
+#           vect = vectorizer.transform(data)
+#           my_prediction = NB_model_loaded.predict(vect)
+#      return render_template('result.html',prediction = my_prediction)
 
 
 @app.route("/data")
